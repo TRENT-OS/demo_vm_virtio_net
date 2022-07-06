b81c77bac631e6a520f1da9d56f1d4ed68f637b9
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-07-06 16:21:20 +0200

    [DEV] SEOS-3351 Reworked CMake to use fixed dtb

--- plat/jetson-nano-2gb-dev-kit/plat.cmake
+++ plat/jetson-nano-2gb-dev-kit/plat.cmake
@@ -1,1 +1,2 @@
 # Make Cmake happy 
+set(VM_INITRD_START 0xCD700000)
--- plat/qemu-arm-virt/plat.cmake
+++ plat/qemu-arm-virt/plat.cmake
@@ -2,6 +2,7 @@
 # Copyright (C) 2022, HENSOLDT Cyber GmbH
 #
 
+set(VM_INITRD_START 0x4d700000)
 set(QEMU_MEMORY "3072M")
 #set(QEMU_MACHINE "foo")
 #set(ARM_CPU .....)
